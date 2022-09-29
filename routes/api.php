c9b3bb75212dcfbe35cfb5f8b6e2704ddd7c5188
mdhamidulislamT <hamidlive6@gmail.com>
2022-09-29 10:39:15 +0600

    student api resource added

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AuthorsController;
 use App\Http\Controllers\BooksController;
+use App\Http\Controllers\StudentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -30,6 +31,7 @@ Route::middleware('auth:api')->prefix('v1')->group(function(){
      */
     Route::apiResource('/authors', AuthorsController::class);
     Route::apiResource('/books', BooksController::class);
+    Route::apiResource('/students', StudentController::class);
 
 });
 
